3af12886c9b3c1ed99a90196cd66fe119a0dc3dc
johnlloyd <yukimiranda26@gmail.com>
2024-06-17 22:03:46 +0800

    save

---
+++ public/js/removinghash.js
@@ -0,0 +1,17 @@
+// $(document).ready(function() {
+//     var lastScrollTop = 0;
+
+//     window.addEventListener("scroll", function() {
+//         var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+//         if (currentScroll > lastScrollTop) {
+//             // Downscroll code
+//             $('.img-fluid').addClass('slide-left');
+//         } else {
+//             // Upscroll code
+//             $('.img-fluid').removeClass('slide-left');
+//         }
+
+//         lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+//     }, false);
+// });
